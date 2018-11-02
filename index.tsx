f504484c50d960faddc46e4b4ae52ae0b8d93f0b
Dominic Cassidy <dcassidy@arago.de>
2018-11-02 10:13:32 +0000

    Stylesheet loaded directly in Provider. Runs ready callback from desktop

--- index.tsx
+++ index.tsx
@@ -1,26 +1,28 @@
 import React from "react";
 import { render } from "react-dom";
-import { Container, Icon, TopBar } from "./src";
+import { Container, Icon, TopBar, HiroApp } from "./src";
 
 const dropdownOptions = [
   { key: "profile", text: "View Profile" },
   { key: "logout", text: "Logout" }
 ];
 
-const Test = () => {
+const Test = ({ ready }) => {
   return (
-    <Container fluid>
-      <TopBar
-        title="Test Page"
-        logo="https://hiro.arago.co/app/images/favicon/android-icon-192x192.png"
-        options={[
-          { key: "profile", text: "View Profile" },
-          { key: "logout", text: "Logout" }
-        ]}
-        trigger={<Icon name="user" />}
-      />
-      <Container>Test</Container>
-    </Container>
+    <HiroApp ready={ready}>
+      <Container fluid>
+        <TopBar
+          title="Test Page"
+          logo="https://hiro.arago.co/app/images/favicon/android-icon-192x192.png"
+          options={[
+            { key: "profile", text: "View Profile" },
+            { key: "logout", text: "Logout" }
+          ]}
+          trigger={<Icon name="user" />}
+        />
+        <Container>Test</Container>
+      </Container>
+    </HiroApp>
   );
 };
 
@@ -32,4 +34,4 @@ if (!target) {
   throw new Error("Failed to find #app");
 }
 
-render(<Test />, target);
+render(<Test ready={console.log("ready!")} />, target);
