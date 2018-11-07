030c07bfbcf734e26b3e7f0ad330052572d0b15a
Dominic Cassidy <dcassidy@arago.de>
2018-11-07 15:17:10 +0000

    Added auth context

---
+++ src/auth/index.ts
@@ -0,0 +1,74 @@
+import { Popup } from "hiro-graph-implicit-oauth";
+import HiroGraphOrm from "hiro-graph-orm";
+import mappings from "hiro-graph-orm-mappings";
+
+export interface IAuthConfig {
+  api: string;
+  url: string;
+  clienId: string;
+}
+
+interface IToken {
+  accessToken: string;
+  meta: any;
+}
+
+interface ILoginResult {
+  ok: boolean;
+  token: string;
+  me?: void | any;
+  orm?: any;
+}
+type AuthCallback = (err: Error, token: any) => void;
+
+const doAuth = (f: (callback: AuthCallback) => void): Promise<ILoginResult> => {
+  return new Promise(resolve => {
+    f(async (err, token: IToken) => {
+      // @ts-ignore
+      let me: void | any;
+      let orm: void | HiroGraphOrm;
+
+      const accessToken = (token || {}).accessToken;
+
+      if (err) {
+        console.error(err);
+      } else if (!!accessToken) {
+        orm = new HiroGraphOrm(
+          {
+            endpoint: "https://stagegraph.arago.co/",
+            token: accessToken
+          },
+          mappings
+        );
+
+        me = await orm.person().catch(console.error);
+      }
+
+      return resolve({
+        me,
+        ok: !err && !!accessToken && !!me,
+        orm,
+        token: accessToken
+      });
+    });
+  });
+};
+
+export class Auth {
+  private check: any;
+  private request: any;
+
+  constructor(config: IAuthConfig) {
+    const { check, request } = Popup(config);
+    this.check = check;
+    this.request = request;
+  }
+
+  isLoggedIn = () => {
+    return doAuth(this.check);
+  };
+
+  login = () => {
+    return doAuth(this.request);
+  };
+}
---
+++ src/components/AuthContext.tsx
@@ -0,0 +1,112 @@
+import React, { Component, createContext } from "react";
+import { Loader } from "semantic-ui-react";
+import { Auth, IAuthConfig } from "../auth";
+
+interface IProps {
+  config: IAuthConfig;
+}
+
+interface IState {
+  ok: boolean;
+  token?: string;
+  me?: any;
+  orm?: any;
+}
+
+export interface IAuthContext {
+  me?: any;
+  orm?: any;
+  token?: string;
+}
+
+const Context = createContext<IAuthContext>({});
+
+export default class AuthProvider extends Component<IProps, IState> {
+  private cancel?: () => void;
+  private auth: Auth;
+
+  constructor(props: IProps) {
+    super(props);
+
+    this.auth = new Auth(props.config);
+
+    this.state = {
+      ok: false
+    };
+  }
+
+  check = () => {
+    let cancel = false;
+
+    this.auth.isLoggedIn().then(async ({ ok, token, orm, me }: IState) => {
+      if (cancel) {
+        return;
+      }
+
+      this.setState({ ok, token, orm, me });
+
+      if (ok) {
+        return;
+      }
+
+      const success = await this.auth.login();
+
+      if (cancel) {
+        return;
+      }
+
+      if (success) {
+        window.location.reload();
+      } else {
+        console.error("Failed to login");
+      }
+    });
+
+    return () => {
+      cancel = true;
+    };
+  };
+
+  componentDidMount() {
+    this.cancel = this.check();
+  }
+
+  componentWillReceiveProps() {
+    this.cancel = this.check();
+  }
+
+  componentWillUnmount() {
+    if (this.cancel && typeof this.cancel === "function") {
+      this.cancel();
+    }
+  }
+
+  render = () => {
+    const { children } = this.props;
+    const { ok } = this.state;
+
+    if (!ok) {
+      return (
+        <div>
+          <Loader active size="huge">
+            Logging in...
+          </Loader>
+        </div>
+      );
+    }
+
+    return (
+      <Context.Provider
+        value={{
+          me: this.state.me,
+          orm: this.state.orm,
+          token: this.state.token
+        }}
+      >
+        {children}
+      </Context.Provider>
+    );
+  };
+}
+
+export const AuthConsumer = Context.Consumer;
