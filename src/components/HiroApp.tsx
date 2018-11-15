f6c377d98ff36d702ecc047f09d3cedfa3e55663
Dominic Cassidy <dcassidy@arago.de>
2018-11-15 11:42:30 +0000

    HiroApp now functional using hooks.

--- src/components/HiroApp.tsx
+++ src/components/HiroApp.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 
 import { IAuthConfig } from "../auth";
@@ -13,77 +13,73 @@ interface IHiroAppProps {
   orm?: any;
   config?: IAuthConfig;
   login?: boolean;
+  children: any;
 }
 
-interface IHiroAppState {
-  colours?: {
-    IThemeColour: string;
-  };
-  me?: any;
-}
+export const HiroApp = ({
+  children,
+  config,
+  login,
+  orm,
+  ready,
+  theme = "default",
+  themeVersion = "latest"
+}: IHiroAppProps) => {
+  const [me, setMe] = useState({});
+  const [colours, setColours] = useState({});
 
-export class HiroApp extends Component<IHiroAppProps, IHiroAppState> {
-  state: IHiroAppState = {};
-
-  componentDidMount() {
-    const { orm, theme = "default", themeVersion = "latest" } = this.props;
+  useEffect(() => {
+    let cancel = false;
 
     fetch(
       `https://dtlv35ikt30on.cloudfront.net/${themeVersion}/${theme}/colours.json`
     )
       .then(res => res.json())
-      .then(colours => {
-        this.setState({ colours });
+      .then(c => {
+        if (cancel) {
+          return;
+        }
+        setColours(c);
       });
 
     if (orm) {
-      orm.person().then(me => this.setState({ me }));
+      orm.person().then(m => setMe(m));
     }
+
+    return () => (cancel = true);
+  }, []);
+
+  const onLoad = () => ready && ready();
+
+  const getColour = (colour: IThemeColour) =>
+    colours ? colours[colour] || "black" : "black";
+
+  let content = children;
+
+  if (login && config) {
+    content = <HiroLogin config={config}>{children}</HiroLogin>;
+  } else if (orm) {
+    content = (
+      <HiroLoginContext.Provider value={{ orm, me }}>
+        {children}
+      </HiroLoginContext.Provider>
+    );
   }
 
-  onLoad = () => this.props.ready && this.props.ready();
-
-  getColour = (colour: IThemeColour) =>
-    this.state.colours ? this.state.colours[colour] || "black" : "black";
-
-  render = () => {
-    const {
-      login,
-      config,
-      children,
-      orm,
-      theme = "default",
-      themeVersion = "latest"
-    } = this.props;
-    const { me } = this.state;
-
-    let content = children;
-
-    if (login && config) {
-      content = <HiroLogin config={config}>{children}</HiroLogin>;
-    } else if (orm) {
-      content = (
-        <HiroLoginContext.Provider value={{ orm, me }}>
-          {children}
-        </HiroLoginContext.Provider>
-      );
-    }
-
-    return (
-      <BrowserRouter>
-        <ThemeContext.Provider
-          value={{
-            getColour: this.state.colours ? this.getColour : () => "transparent"
-          }}
-        >
-          <link
-            rel="stylesheet"
-            href={`https://dtlv35ikt30on.cloudfront.net/${themeVersion}/${theme}/semantic.min.css`}
-            onLoad={this.onLoad}
-          />
-          {content}
-        </ThemeContext.Provider>
-      </BrowserRouter>
-    );
-  };
-}
+  return (
+    <BrowserRouter>
+      <ThemeContext.Provider
+        value={{
+          getColour: colours ? getColour : () => "transparent"
+        }}
+      >
+        <link
+          rel="stylesheet"
+          href={`https://dtlv35ikt30on.cloudfront.net/${themeVersion}/${theme}/semantic.min.css`}
+          onLoad={onLoad}
+        />
+        {content}
+      </ThemeContext.Provider>
+    </BrowserRouter>
+  );
+};
