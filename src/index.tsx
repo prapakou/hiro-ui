e95fa227f7816400f5025e9f4300bdec0544fd1c
Dominic Cassidy <dcassidy@arago.de>
2018-11-23 12:48:43 +0000

    Export state subscribe component from unstated

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ export * from "semantic-ui-react";
 export * from "./components";
 export * from "./stores";
 
-export { Container as StateContainer } from "unstated";
+export {
+  Container as StateContainer,
+  Subscribe as StateSubscribe
+} from "unstated";
 
 export { default as subscribe } from "./subscribe";
