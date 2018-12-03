94fcfc2e74db44f30cd20d9fd917a270504da2f9
Dominic Cassidy <dcassidy@arago.de>
2018-12-03 13:10:03 +0000

    Use throttleTime to limit login checks

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,5 +1,5 @@
 import { BehaviorSubject, Subject } from "rxjs";
-import { catchError, filter, switchMap, tap } from "rxjs/operators";
+import { catchError, filter, switchMap, throttleTime } from "rxjs/operators";
 
 import { Auth } from "../auth";
 import { createStateGetter } from "../helpers";
@@ -36,7 +36,6 @@ const getOrSet = (key: AuthKeys, value: any) => {
 const store = new Map<AuthKeys, any>();
 const authCheck$ = new Subject<IAuthCheck>();
 const auth$ = new BehaviorSubject<IAuthState>({});
-let busy = false;
 
 let auth;
 
@@ -50,11 +49,9 @@ class LoginError extends Error {
 // Subscriptions
 authCheck$
   .pipe(
-    filter(() => !busy),
-    tap(() => (busy = true)),
     filter(v => !!v.authConfig || (!!v.config && !!v.orm)),
+    throttleTime(2000),
     switchMap(async v => {
-      console.log("ENSURING LOGIN", 2);
       if (v.orm && v.config) {
         const me = await v.orm.person();
         return { me, orm: v.orm, token: v.config.token, updated: new Date() };
@@ -81,17 +78,15 @@ authCheck$
     catchError(err => {
       errorStore.actions.setError(err);
       return [{}];
-    }),
-    tap(() => (busy = false))
+    })
   )
   .subscribe(auth$);
 
-const ensureLogin = async (newAuthConfig?, newConfig?, newOrm?) => {
+const ensureLogin = (newAuthConfig?, newConfig?, newOrm?) => {
   const orm = getOrSet("orm", newOrm);
   const config = getOrSet("config", newConfig);
   const authConfig = getOrSet("authConfig", newAuthConfig);
 
-  console.log("ENSURING LOGIN", newAuthConfig, authConfig);
   authCheck$.next({ orm, config, authConfig });
 };
 
