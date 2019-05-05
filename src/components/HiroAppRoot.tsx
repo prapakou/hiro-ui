1fed1d2dca08abcb9c5322a886576c8ba9dc0c45
Dominic Cassidy <dcassidy@arago.de>
2019-05-05 10:14:34 +0100

    Remove default container

--- src/components/HiroAppRoot.tsx
+++ src/components/HiroAppRoot.tsx
@@ -10,25 +10,18 @@ import { HiroGraph, HiroGraphConfig } from "./HiroGraph";
 interface HiroAppRootProps {
   auth?: HiroGraphConfig;
   children?: any;
-  className?: string;
-  fluid?: boolean;
-  ready?: () => void;
   store?: Store;
 }
 
 export const HiroAppRoot: React.FC<HiroAppRootProps> = ({
   auth,
   children,
-  className,
-  fluid,
   store
 }) => (
   <BrowserRouter>
     <StoreContext.Provider value={store}>
-      <Container fluid={fluid} className={className}>
-        {auth ? <HiroGraph config={auth}>{children}</HiroGraph> : children}
-        <ErrorBar />
-      </Container>
+      {auth ? <HiroGraph config={auth}>{children}</HiroGraph> : children}
+      <ErrorBar />
     </StoreContext.Provider>
   </BrowserRouter>
 );
