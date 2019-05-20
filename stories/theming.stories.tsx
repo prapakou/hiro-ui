1eca832c11c3d5784092ee472d9aad088ccdaf5b
Dominic Cassidy <dcassidy@arago.de>
2019-05-20 13:58:42 +0100

    Enable changing themes in theme storybook

--- stories/theming.stories.tsx
+++ stories/theming.stories.tsx
@@ -1,19 +1,61 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { storiesOf } from "@storybook/react";
 
-import { Container, HiroTheme } from "../src";
+import { Container, HiroTheme, Dropdown, Menu } from "../src";
 
 import { SiteDemo } from "./theming/site";
 import { MenuDemo } from "./theming/menu";
 import { ButtonsDemo } from "./theming/buttons";
 import { TableDemo } from "./theming/tables";
 
-storiesOf("Themes", module).add("Demo", () => (
-  <Container fluid>
-    <HiroTheme.Default />
-    <SiteDemo />
-    <MenuDemo />
-    <ButtonsDemo />
-    <TableDemo />
-  </Container>
-));
+const ThemeController = () => {
+  const theme = sessionStorage.getItem("theme") || "default";
+
+  const changeTheme = useCallback((_, { value }) => {
+    sessionStorage.setItem("theme", value);
+    window.location.reload();
+  }, []);
+
+  let content = <HiroTheme.Default />;
+
+  if (theme === "portal") {
+    content = <HiroTheme.Portal />;
+  }
+
+  if (theme === "saas") {
+    content = <HiroTheme.SAAS />;
+  }
+
+  return (
+    <>
+      <Menu borderless secondary>
+        <Menu.Item position="right">
+          <span style={{ paddingRight: "10px" }}>Theme</span>
+          <Dropdown
+            options={[
+              { text: "Default", value: "default" },
+              { text: "SAAS", value: "saas" },
+              { text: "Portal", value: "portal" }
+            ]}
+            onChange={changeTheme}
+            selection
+            value={theme}
+          />
+        </Menu.Item>
+      </Menu>
+      {content}
+    </>
+  );
+};
+
+storiesOf("Themes", module).add("Demo", () => {
+  return (
+    <Container fluid>
+      <ThemeController />
+      <SiteDemo />
+      <MenuDemo />
+      <ButtonsDemo />
+      <TableDemo />
+    </Container>
+  );
+});
